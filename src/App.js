4feb2a020446526b47939346e05209cd84162fa3
jcarreazo <jcarreazos.dev@gmail.com>
2023-02-12 18:35:34 -0500

    Accesing-the-meme-Api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './style.css';
 import Header from './Header.js';
 import Buttons from './Buttons.js';
-import Image from './Image.js';
 export default function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Buttons />
-      <Image />
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,19 +1,38 @@
 import React from 'react';
-
+var url = '';
+let array = '';
 export default function Buttons(props) {
-  function handle() {
-    console.log('IS OVER ANAKIN!');
+  async function fetchingSteam() {
+    const response = await fetch('https://api.imgflip.com/get_memes');
+    const movies = await response.json();
+    return movies;
+  }
+
+  async function handle() {
+    fetchingSteam().then((movies) => {
+      array = movies.data.memes;
+    });
+    fetchingSteam().then((movies) => {
+      array = movies.data.memes;
+    });
+
+    const random = Math.floor(Math.random() * array.length);
+    url = array[random].url;
+    console.log(url);
   }
   return (
-    <div>
+    <div className="App">
       <div className="ButtonsContainer">
         <div className="InputContainer">
           <input type="text" className="Input-1" />
           <input type="text" className="Input-2" />
         </div>
-        <button onMouseEnter={handle} className="SubmitButton">
+        <button onClick={handle} className="SubmitButton">
           "Get a new meme image 🖼️"
         </button>
+      </div>
+      <div className="ImageContainer">
+        <img src={url} alt="meme" className="Image" />
       </div>
     </div>
   );
